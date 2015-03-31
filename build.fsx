eece92fbe9e869a54e18c49df3cad6d654132cd6
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-03-31 19:42:35 +0200

    Version 3.0 stable

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.Formlets", "3.0-alpha")
+    BuildTool().PackageId("WebSharper.Formlets", "3.0")
     |> fun bt -> bt.WithFramework(bt.Framework.Net40)
 
 let if_formlets =
